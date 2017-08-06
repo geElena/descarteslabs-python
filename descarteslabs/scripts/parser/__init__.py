8e0c425baa8d87a94378fc08495954f389b3ba4d
aliasmrchips <aliasmrchips@descarteslabs.com>
2017-08-06 02:12:54 +0000

    flake8

--- descarteslabs/scripts/parser/__init__.py
+++ descarteslabs/scripts/parser/__init__.py
@@ -54,7 +54,8 @@ metadata_parser.add_argument('-bbox', help='Whether or not to use a bounding box
 
 # Notification Group
 notification_parser = subparsers.add_parser('notification')
-notification_parser.add_argument('command', choices=['identify', 'upload', 'file', 'shape', 'update', 'delete'], help='The action to take')
+notification_parser.add_argument('command', choices=['identify', 'upload', 'file', 'shape', 'update', 'delete'],
+                                 help='The action to take')
 notification_parser.add_argument('-url', help='The url of the service')
 notification_parser.add_argument('argument', nargs='?')
 notification_parser.add_argument('-file_id', default=None, help='The file id')
