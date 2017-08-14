2ad5e2c26fa05c85d02b5d6590486a47cefcda3a
aliasmrchips <aliasmrchips@descarteslabs.com>
2017-08-14 19:45:10 +0000

    Update tests

--- descarteslabs/tests/test_places.py
+++ descarteslabs/tests/test_places.py
@@ -59,15 +59,15 @@ class TestPlaces(unittest.TestCase):
 
     def test_sources(self):
         r = self.instance.sources()
-        self.assertIn('nass', r)
+        self.assertEqual(2, len(r))
 
     def test_categories(self):
         r = self.instance.categories()
-        self.assertIn('corn', r)
+        self.assertEqual(2, len(r))
 
     def test_metrics(self):
         r = self.instance.metrics()
-        self.assertIn('yield', r)
+        self.assertEqual(4, r)
 
     def test_value(self):
         r = self.instance.value('north-america_united-states', source='nass', category='corn', metric='yield')
