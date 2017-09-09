263113770abf648e7c3b0bb031d2bad6c2e63348
Sam Skillman <samskillman@gmail.com>
2017-09-09 09:37:37 -0600

    Extend descarteslabs namespace for py3 compat

--- descarteslabs/__init__.py
+++ descarteslabs/__init__.py
@@ -14,6 +14,7 @@
 
 # flake8: noqa
 
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 __version__ = "0.4.6"
 from .auth import Auth
 descartes_auth = Auth.from_environment_or_token_json()
